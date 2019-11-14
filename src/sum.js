91a539d6398716c26eb0d4017674ebddd86b1bc2
John Goytia <JGoytia@ucvts.tec.nj.us>
2019-11-14 13:17:47 -0500

    commit 2 - worked on sum

---
+++ src/sum.js
@@ -0,0 +1,12 @@
+const readlineSync = require("readline-Sync");
+const lowBound = Number(readlineSync.question("Lower bound: "));
+const upperBound = Number(readlineSync.question("Upper bound: "));
+
+if (lowBound < Number.MIN_SAFE_INTEGER || upperBound > Number.MAX_SAFE_INTERGER || lowerBound < upperBound) {
+  const readlineSync = require("readline-Sync");
+  const lowBound = Number(readlineSync.question("Lower bound: "));
+  const upperBound = Number(readlineSync.question("Upper bound: "));
+}else if (upperBound % 2 == 0){
+
+}else if (upperBound - 1)% 2 == 0)
+  while ()
