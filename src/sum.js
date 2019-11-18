718b9a795d163951d3daaf54798b9cd7efe700ef
John Goytia <JGoytia@ucvts.tec.nj.us>
2019-11-18 12:58:53 -0500

    commit 4 - exercise 1 in progess

--- src/sum.js
+++ src/sum.js
@@ -1,12 +1,29 @@
 const readlineSync = require("readline-Sync");
+
+let sum = 0;
+let x = 0;
+
+console.log("");
+
 const lowBound = Number(readlineSync.question("Lower bound: "));
 const upperBound = Number(readlineSync.question("Upper bound: "));
 
-if (lowBound < Number.MIN_SAFE_INTEGER || upperBound > Number.MAX_SAFE_INTERGER || lowerBound < upperBound) {
-  const readlineSync = require("readline-Sync");
-  const lowBound = Number(readlineSync.question("Lower bound: "));
-  const upperBound = Number(readlineSync.question("Upper bound: "));
-}else if (upperBound % 2 == 0){
+do{
+  lowBound = Number(readlineSync.question("Lower bound: "));
+  upperBound = Number(readlineSync.question("Upper bound: "));
+} while (lowBound < Number.MIN_SAFE_INTEGER || upperBound > Number.MAX_SAFE_INTEGER || lowBound >= upperBound || Number.isNaN(lowBound) || Number.isNaN(upperBound) || lowBound % 1 != 0 || upperBound % 1 != 0)
 
-}else if (upperBound - 1)% 2 == 0)
-  while ()
+for (let x = lowBound; x <= upperBound; x++) {
+  if (lowBound % 2 === 0){
+    for (x = lowBound; x <= upperBound; x+=2){
+      sum = x + sum;
+    }
+  }
+}
+if (lowBound % 2 !== 0){
+  for (x = lowBound + 1; x <= upperBound; x+=2){
+    sum = x + sum;
+  }
+}
+sum = sum.toLocaleString("en");
+console.log("\n" + sum + ".");
