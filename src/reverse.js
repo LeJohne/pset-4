65966e6acf9d7433fc4eacfbe30057a0b3cfc5d8
LeJohne <johngoy11024@gmail.com>
2019-11-24 19:35:54 -0500

    commit 7 - exercise 2 done

---
+++ src/reverse.js
@@ -0,0 +1,24 @@
+const readlineSync = require("readline-sync");
+
+const MIN = 1;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+let positive = Number(readlineSync.question("\nPositive integer: "));
+let reverse = "";
+let number;
+
+while (positive < MIN || positive > MAX || Number.isNaN(positive) == true || Number.isInteger(positive) == false) {
+  positive = Number(readlineSync.question("Positive integer: "));
+}
+
+while (positive > 0.1) {
+  number = positive % 10;
+  positive = Math.floor(positive / 10);
+  if (positive >= 1) {
+    reverse = reverse + number + ", ";
+  }
+  else {
+    reverse = reverse + number + ".\n";
+  }
+}
+console.log("\n" + reverse);
