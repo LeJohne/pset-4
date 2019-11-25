7488e04050c78e982b23d36607f2d18cdf9f3f37
LeJohne <johngoy11024@gmail.com>
2019-11-24 23:39:07 -0500

    commit 10 - finalization

--- src/sum.js
+++ src/sum.js
@@ -1,25 +1,25 @@
 const readlineSync = require("readline-Sync");
 
 let sum = 0;
-let x = 0;
+let a = 0;
 
 console.log(""); //maybe add /n
 
 do{
   lowBound = Number(readlineSync.question("Lower bound: "));
   upperBound = Number(readlineSync.question("Upper bound: "));
-} while (lowBound < Number.MIN_SAFE_INTEGER || upperBound > Number.MAX_SAFE_INTEGER || lowBound >= upperBound || Number.isNaN(lowBound) || Number.isNaN(upperBound) || lowBound % 1 != 0 || upperBound % 1 != 0)
+} while (lowBound < Number.MIN_SAFE_INTEGER || upperBound > Number.MAa_SAFE_INTEGER || lowBound >= upperBound || Number.isNaN(lowBound) || Number.isNaN(upperBound) || lowBound % 1 != 0 || upperBound % 1 != 0)
 
-for (let x = lowBound; x <= upperBound; x++) {
+for (let a = lowBound; a <= upperBound; a++) {
   if (lowBound % 2 === 0){
-    for (x = lowBound; x <= upperBound; x+=2){
-      sum = x + sum;
+    for (a = lowBound; a <= upperBound; a+=2){
+      sum = a + sum;
     }
   }
 }
 if (lowBound % 2 !== 0){
-  for (x = lowBound + 1; x <= upperBound; x+=2){
-    sum = x + sum;
+  for (a = lowBound + 1; a <= upperBound; a+=2){
+    sum = a + sum;
   }
 }
 sum = sum.toLocaleString("en");
